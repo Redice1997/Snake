64000b2e22dc5de33a6b3119421dba215f96651c
Redice1997 <roshin-vladimir@list.ru>
2021-12-30 18:18:05 +0300

    Создал змейку, переделал старые функции, функция поедания пока работает некорректно

--- Snake/Form1.Designer.cs
+++ Snake/Form1.Designer.cs
@@ -30,37 +30,66 @@ namespace Snake
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.Cube = new System.Windows.Forms.PictureBox();
+            this.snakeHead = new System.Windows.Forms.PictureBox();
             this.timer = new System.Windows.Forms.Timer(this.components);
-            ((System.ComponentModel.ISupportInitialize)(this.Cube)).BeginInit();
+            this.fruit = new System.Windows.Forms.PictureBox();
+            this.labelScore = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.snakeHead)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fruit)).BeginInit();
             this.SuspendLayout();
             // 
-            // Cube
+            // snakeHead
             // 
-            this.Cube.BackColor = System.Drawing.SystemColors.Highlight;
-            this.Cube.Location = new System.Drawing.Point(0, 0);
-            this.Cube.Name = "Cube";
-            this.Cube.Size = new System.Drawing.Size(40, 40);
-            this.Cube.TabIndex = 0;
-            this.Cube.TabStop = false;
+            this.snakeHead.BackColor = System.Drawing.SystemColors.Highlight;
+            this.snakeHead.Location = new System.Drawing.Point(0, 0);
+            this.snakeHead.Name = "snakeHead";
+            this.snakeHead.Size = new System.Drawing.Size(40, 40);
+            this.snakeHead.TabIndex = 0;
+            this.snakeHead.TabStop = false;
+            // 
+            // fruit
+            // 
+            this.fruit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.fruit.Location = new System.Drawing.Point(0, 40);
+            this.fruit.Name = "fruit";
+            this.fruit.Size = new System.Drawing.Size(40, 40);
+            this.fruit.TabIndex = 1;
+            this.fruit.TabStop = false;
+            // 
+            // labelScore
+            // 
+            this.labelScore.AutoSize = true;
+            this.labelScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelScore.Location = new System.Drawing.Point(810, 10);
+            this.labelScore.Name = "labelScore";
+            this.labelScore.Size = new System.Drawing.Size(63, 17);
+            this.labelScore.TabIndex = 2;
+            this.labelScore.Text = "Счёт: 0";
+            this.labelScore.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(384, 361);
-            this.Controls.Add(this.Cube);
+            this.ClientSize = new System.Drawing.Size(884, 761);
+            this.Controls.Add(this.labelScore);
+            this.Controls.Add(this.fruit);
+            this.Controls.Add(this.snakeHead);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.Cube)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.snakeHead)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fruit)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox Cube;
+        private System.Windows.Forms.PictureBox snakeHead;
         private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.PictureBox fruit;
+        private System.Windows.Forms.Label labelScore;
     }
 }
 
--- Snake/Form1.cs
+++ Snake/Form1.cs
@@ -18,25 +18,125 @@ namespace Snake
         private int dirX = 0, dirY = 0;
         private int sizeOfSides = 40;
 
+        private List<PictureBox> snake = new List<PictureBox>();
+
+        private short score = 0;
+
         public Form1()
         {
             InitializeComponent();
             this.Width = WIDTH;
             this.Height = HEIGHT;
-            GenerateMap();
-            timer.Tick += new EventHandler(DoNextStep);
+
+            snake.Add(snakeHead);
+
+            GenerateMap();            
+
+            timer.Tick += new EventHandler(MoveSnake);
+            timer.Tick += new EventHandler(FindFruit);
             timer.Interval = 500;
             timer.Start();
-            KeyDown += new KeyEventHandler(MoveCube);            
-        }              
+
+            KeyDown += new KeyEventHandler(SetNewDir);            
+        }  
         
+        private void GenerateFruit()
+        {
+            Random r = new Random();
+            Point point = fruit.Location;
+            bool isOutOfSnake = false;
+
+            while (!isOutOfSnake)
+            {
+                point.X = r.Next(0, Width / sizeOfSides - 2) * sizeOfSides;
+                point.Y = r.Next(0, Height / sizeOfSides - 1) * sizeOfSides;
+                isOutOfSnake = true;
+
+                foreach (PictureBox cube in snake)
+                {
+                    if (point == cube.Location)
+                        isOutOfSnake = false;
+                }
+            }           
+            
+            fruit.Location = point;
+        }      
+        
+
+        private void MoveSnake(object sender, EventArgs e)
+        {
+            Point nextStep = new Point(snakeHead.Location.X + sizeOfSides * dirX, snakeHead.Location.Y + sizeOfSides * dirY);            
+            for (int i = 1; i < snake.Count; i++)
+            {
+                snake[i].Location = snake[i - 1].Location;
+            }
+            snakeHead.Location = nextStep;
+        }
+
+        //private void MoveCube(object sender, EventArgs e)
+        //{
+        //    snakeHead.Location = new Point(snakeHead.Location.X + sizeOfSides * dirX, snakeHead.Location.Y + sizeOfSides * dirY);
+        //}
+
+        private void FindFruit(object sender, EventArgs e)
+        {
+            if (fruit.Location == snakeHead.Location) EatFruit();            
+        }
+
+        private void EatFruit()
+        {            
+            GenerateFruit();
+            PictureBox body = new PictureBox();
+            body.Size = new Size(40, 40);
+            body.BackColor = snakeHead.BackColor;
+            body.Location = snake[snake.Count - 1].Location;
+            snake.Add(body);
+            score++;
+            labelScore.Text = $"Счёт: {score}";            
+        }
+
+
+        private void SetNewDir(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode.ToString())
+            {
+                case "Up":
+                    if (dirY != 1)
+                    {
+                        dirY = -1;
+                        dirX = 0;
+                    }                    
+                    break;
+                case "Down":
+                    if (dirY != -1)
+                    {
+                        dirY = 1;
+                        dirX = 0;
+                    }                    
+                    break;
+                case "Left":
+                    if (dirX != 1)
+                    {
+                        dirY = 0;
+                        dirX = -1;
+                    }                    
+                    break;
+                case "Right":
+                    if (dirX != -1)
+                    {
+                        dirY = 0;
+                        dirX = 1;
+                    }
+                    break;
+            }            
+        }
         private void GenerateMap()
         {
             for (int i = 0; i <= Height / sizeOfSides; i++)
             {
                 PictureBox blackLine = new PictureBox();
                 blackLine.BackColor = Color.Black;
-                blackLine.Location = new Point(i * Cube.Size.Width, 0);
+                blackLine.Location = new Point(i * sizeOfSides, 0);
                 blackLine.Size = new Size(1, Height);
                 Controls.Add(blackLine);
             }
@@ -44,39 +144,11 @@ namespace Snake
             {
                 PictureBox blackLine = new PictureBox();
                 blackLine.BackColor = Color.Black;
-                blackLine.Location = new Point(0, i * Cube.Size.Width);
+                blackLine.Location = new Point(0, i * sizeOfSides);
                 blackLine.Size = new Size(Width - 100, 1);
                 Controls.Add(blackLine);
             }
         }
 
-        private void DoNextStep(object sender, EventArgs e)
-        {
-            Cube.Location = new Point(Cube.Location.X + sizeOfSides * dirX, Cube.Location.Y + sizeOfSides * dirY);
-        }
-
-        private void MoveCube(object sender, KeyEventArgs e)
-        {
-            switch (e.KeyCode.ToString())
-            {
-                case "Up":
-                    dirY = -1;
-                    dirX = 0;
-                    break;
-                case "Down":
-                    dirY = 1;
-                    dirX = 0;
-                    break;
-                case "Left":
-                    dirY = 0;
-                    dirX = -1;
-                    break;
-                case "Right":
-                    dirY = 0;
-                    dirX = 1;
-                    break;
-            }
-        }
-       
     }
 }
